f27a00bdd5038c42419a6cba1ab1091a615f8d4a
Your Actual Name <Your Actual Email>
2021-01-03 00:09:30 +0900

    fix: eslintに怒られていた

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,22 @@
-const path = require('path');
+import { resolve } from 'path';
 
-module.exports = {
-  mode: process.env.NODE_ENV || 'development',
-  entry: './src/app.ts',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(j|t)s$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              '@babel/preset-env',
-              '@babel/preset-typescript'
-            ]
-          }
+export const mode = process.env.NODE_ENV || 'development';
+export const entry = './src/app.ts';
+export const output = {
+  filename: 'main.js',
+  path: resolve(__dirname, 'dist')
+};
+export const module = {
+  rules: [
+    {
+      test: /\.(j|t)s$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-typescript']
         }
       }
-    ]
-  },
+    }
+  ]
 };
