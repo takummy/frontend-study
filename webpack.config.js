cedd50e9d62cb689030cba9b7a89f45e55a13e5f
Your Actual Name <Your Actual Email>
2021-01-13 00:02:12 +0900

    React導入

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,23 @@
-import { resolve } from 'path';
+import path from 'path';
 
 export const mode = process.env.NODE_ENV || 'development';
 export const entry = './src/app.ts';
 export const output = {
   filename: 'main.js',
-  path: resolve(__dirname, 'dist')
+  path: path.resolve(__dirname, 'dist')
+};
+export const resolve = {
+  extensions: ['.js', '.ts', '.tsx']
 };
 export const module = {
   rules: [
     {
-      test: /\.(j|t)s$/,
+      test: /\.(j|t)sx?$/,
       exclude: /node_modules/,
       use: {
         loader: 'babel-loader',
         options: {
-          presets: ['@babel/preset-env', '@babel/preset-typescript']
+          presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript']
         }
       }
     }
